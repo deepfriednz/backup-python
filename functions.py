9d670baf4f7bd94465bfa32d96328b04790306e3
louistan3 <louistan3@gmail.com>
2019-05-12 00:39:57 +1200

    add basic functions

--- functions.py
+++ functions.py
@@ -1,10 +1,51 @@
+import os
+import nacl.secret
+import nacl.utils
+import nacl.pwhash
+import base64
+
+
 def traverse(dirs):
     for root, dirs, files in os.walk(dirs):
         for file in files:
             if file.endswith(".txt"):
                 print(os.path.join(root, file))
 
-def get_files(work_dir: object) -> object:
-    file_list = os.scandir(work_dir)
-    for the_file in file_list:
-        print(the_file)
+
+def get_files(work_dir: object):
+    scan_list = os.scandir(work_dir)
+    file_list = []
+
+    for the_file in scan_list:
+        file_list.append(os.path.realpath(the_file))
+
+    return file_list
+
+
+def generate_enc_key(salt: object, password: object):
+    derived_key = nacl.pwhash.argon2i.kdf(nacl.secret.SecretBox.KEY_SIZE, password, salt)
+    return derived_key
+
+
+def encrypt_object(file: object, enc_key):
+
+    box = nacl.secret.SecretBox(enc_key)
+    encrypted = box.encrypt(file)
+    return encrypted
+
+
+def encode_base64(file: object):
+    encoded = base64.b64encode(file).decode("ascii")
+    return encoded
+
+
+def read_in_chunks(file_object, chunk_size=32 * 1024):
+    """Lazy function (generator) to read a file piece by piece.
+    Default chunk size: 16k."""
+    index = 0
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            break
+        yield (data, index)
+        index += 1
--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import os
 import functions
+import nacl.utils
+import nacl.pwhash
 
-
-BACKUP_DIRS = "dirs.conf"
+BACKUP_DIRS = open("dirs.conf")
+PASSWORD = "deepfriednz!".encode("utf-8")
+SALT = nacl.utils.random(nacl.pwhash.argon2i.SALTBYTES)
 
 
 def main() -> object:
     print("Backup script")
+
+    print("Create key from password file...")
+    enc_key = functions.generate_enc_key(SALT, PASSWORD)
+
+    secret_msg = b"This is gonna be good..."
+    encrypted_object = functions.encrypt_object(secret_msg, enc_key)
+
+    files_to_backup = []
+    encrypted_files = []
+
+    for dirs in BACKUP_DIRS:
+        files_to_backup.append(functions.get_files(dirs))
+
+    for files in files_to_backup:
+        encrypted_files.append(functions.encrypt_object(files, enc_key))
+
+    print(files_to_backup)
+    print(encrypted_files)
 
 
 
